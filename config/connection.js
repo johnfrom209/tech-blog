44273e2292ffa40e3fe822248d7082bcbb175489
Jonathan Moreno <johnfrom209@gmail.com>
2022-11-09 00:00:06 -0800

    RK

--- config/connection.js
+++ config/connection.js
@@ -4,8 +4,6 @@ require('dotenv').config();
 let sequelize;
 
 
-//mysql://hbfgt5nfd9vq2ev5:g346j9mkquvjg71c@o2olb7w3xv09alub.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/dl9wk1svvsnzf2gl
-
 if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWSDB_URL);
     module.exports = sequelize;
